0b6e3763c0aaf7c4fe8450e2c8797b2894936ee0
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-04-03 16:43:44 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -12,7 +12,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-    if !Backer.include(self)
+    if !Backer.backed_projects.include?(self)
       backer.back_project(self)
     end
   end
